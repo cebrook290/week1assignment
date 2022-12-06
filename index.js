976773f51ba279d37101d800bc1ceaebabe5c8b1
Chris Esterbrook <116049806+cebrook290@users.noreply.github.com>
2022-12-06 04:06:17 +0000

    continue to code along

--- index.js
+++ index.js
@@ -15,8 +15,10 @@ console.log(4 - 2);
 console.log(6 * 7);
 console.log(8 / 2);
 
-console.log(bankAccountBalance = 30);
-console.log(bankAccountBalance = numberOfFriends);
+console.log(bankAccountBalance - 30);
+console.log(bankAccountBalance * numberOfFriends);
 
 console.log(5 == 5);
-console.log(5 == 3);
+console.log(5 == 6);
+console.log(bankAccountBalance == numberOfFriends);
+console.log(bankAccountBalance == 52.10);
